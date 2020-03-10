357cd512223ec68bea61cab9f8ae277b5a207b4d
anthony mcmullen <amcmullen9291@gmail.com>
2020-03-10 08:19:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,39 @@ def find_the_cheese(words)
     elsif "#{cheese}" == "gouda"
       return "gouda"
     elsif"#{cheese}" == "camembert"
+      return "camembert"def roll_call_dwarves(dwarves)
+  number = 1
+  i=0
+  while i<dwarves.length
+  dwarves.each do |dwarves|
+  puts  "#{number} , #{dwarves}"
+  number+=1
+  i+=1
+  end 
+  end 
+end
+
+def summon_captain_planet(names)
+  names.map {|name| name.capitalize + "!"}
+end
+
+def long_planeteer_calls(calls)
+  calls.any?{|word| word.length>4}
+end
+
+def find_the_cheese(words)
+  i=0 
+  while i<words.length
+  words.collect {|cheese| if "#{cheese}" == "cheddar"
+      return "cheddar"
+    elsif "#{cheese}" == "gouda"
+      return "gouda"
+    elsif"#{cheese}" == "camembert"
       return "camembert"
+    end
+    }
+  end
+end
     end
     }
   end
